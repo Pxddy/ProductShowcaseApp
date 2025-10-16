8882b19826d4696462b5f92f2c08e174e21c1dd0
Pxddy <patrick.henke@t-online.de>
2025-10-16 21:06:12 +0200

    - added a Room type converter for `List<String>` - enabled the converter in `AppDatabase`

--- app/src/main/kotlin/de/ph/productshowcaseapp/data/local/AppDatabase.kt
+++ app/src/main/kotlin/de/ph/productshowcaseapp/data/local/AppDatabase.kt
@@ -2,8 +2,10 @@ package de.ph.productshowcaseapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [ProductEntity::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDao
 }
