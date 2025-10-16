61b172402470e8df84db0c8d03126d13064545b9
Pxddy <patrick.henke@t-online.de>
2025-10-16 21:33:00 +0200

    - added @Serializable and @SerialName to data classes - added kotlinx.serialization lib

--- app/src/main/kotlin/de/ph/productshowcaseapp/data/remote/ProductDto.kt
+++ app/src/main/kotlin/de/ph/productshowcaseapp/data/remote/ProductDto.kt
@@ -1,12 +1,31 @@
 package de.ph.productshowcaseapp.data.remote
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ProductDto(
+    @SerialName("id")
     val id: Int,
+
+    @SerialName("title")
     val title: String,
+
+    @SerialName("description")
     val description: String,
+
+    @SerialName("price")
     val price: Double,
+
+    @SerialName("rating")
     val rating: Double,
+
+    @SerialName("category")
     val category: String,
+
+    @SerialName("thumbnail")
     val thumbnail: String,
+
+    @SerialName("images")
     val images: List<String>
 )
--- app/src/main/kotlin/de/ph/productshowcaseapp/data/remote/ProductListResponse.kt
+++ app/src/main/kotlin/de/ph/productshowcaseapp/data/remote/ProductListResponse.kt
@@ -1,8 +1,19 @@
 package de.ph.productshowcaseapp.data.remote
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ProductListResponse(
+    @SerialName("products")
     val products: List<ProductDto>,
+
+    @SerialName("total")
     val total: Int,
+
+    @SerialName("skip")
     val skip: Int,
+
+    @SerialName("limit")
     val limit: Int
 )
